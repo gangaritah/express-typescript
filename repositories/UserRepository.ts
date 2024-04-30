2bd4bd7f9feb54e5aad7cc6f25cecbd0ddd40f08
German Angarita <gangaritah@gmail.com>
2024-04-30 06:46:00 -0500

    ADD login in User Repository

--- repositories/UserRepository.ts
+++ repositories/UserRepository.ts
@@ -1,5 +1,8 @@
 import db from '../config/config-db';
 import User from '../Dto/UserDto';
+import Auth from '../Dto/AuthDto';
+import bcrypt from 'bcryptjs';
+
 
 class UserRepository {
 
@@ -7,6 +10,20 @@ class UserRepository {
         const sql = 'INSERT INTO users (email, nombres, apellidos, telefono, password) VALUES (?, ?, ?, ?, ?)';
         const values = [user.email, user.nombres, user.apellidos, user.telefono, user.password];
         return db.execute(sql, values);
+    }
+
+    static async login(auth: Auth){
+        const sql = 'SELECT password FROM users WHERE email=?';
+        const values = [auth.email];
+        const result: any = await db.execute(sql, values);
+        if (result[0].length > 0){
+          const isPasswordValid = await bcrypt.compare(auth.password, result[0][0].password);
+          if (isPasswordValid){
+            return {logged: true, status: "Successful authentication"}
+          }
+          return {logged: false, status: "Invalid username or password" };
+        }
+        return {logged: false, status: "Invalid username or password" };
     }
 }
 
