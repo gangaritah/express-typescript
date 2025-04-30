6fd52a0a6684bf2226c9811c060e3c38699f6ca8
German <German2025@gmail.com>
2025-04-30 16:04:12 -0500

    ADD Validators

--- routes/register.ts
+++ routes/register.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import registerController from '../controllers/register-controller';
+import {validatorParams, validator} from '../middleware/register-validator';
+
 const router = express.Router();
 
 
-router.post('/', registerController);
+router.post('/', validatorParams, validator, registerController);
 
 
 export default router;
